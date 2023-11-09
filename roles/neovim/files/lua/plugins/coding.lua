e9bbb5667bd8c4a6b2a0d2f3784a1bb69f7f92c5
Hogan McDonald <hogan.developer@gmail.com>
2023-11-09 12:35:46 -0600

    updates indent-blankline

--- roles/neovim/files/lua/plugins/coding.lua
+++ roles/neovim/files/lua/plugins/coding.lua
@@ -164,24 +164,9 @@ return {
   -- indent guides for Neovim
   {
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     event = { 'BufReadPost', 'BufNewFile' },
-    opts = {
-      char = '│',
-      filetype_exclude = {
-        'help',
-        'alpha',
-        'dashboard',
-        'neo-tree',
-        'Trouble',
-        'lazy',
-        'mason',
-        'notify',
-        'toggleterm',
-        'lazyterm',
-      },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
+    opts = {},
   },
 
   -- auto pairs
