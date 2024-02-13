7adb20d7921d0724ef16cd00a9f4f01c0792f985
Hogan McDonald <hogan.developer@gmail.com>
2024-02-13 16:50:33 -0600

    wip - replace coq with cmp

--- roles/neovim/files/lua/plugins/coding.lua
+++ roles/neovim/files/lua/plugins/coding.lua
@@ -3,6 +3,84 @@ local bg = require('util.highlight').bg
 
 return {
   -- completion
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = {
+  --     -- {
+  --     --   -- snippet plugin
+  --     --   "L3MON4D3/LuaSnip",
+  --     --   dependencies = "rafamadriz/friendly-snippets",
+  --     --   opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+  --     --   config = function(_, opts)
+  --     --     require("plugins.configs.others").luasnip(opts)
+  --     --   end,
+  --     -- },
+  --
+  --     -- autopairing of (){}[] etc
+  --     {
+  --       "windwp/nvim-autopairs",
+  --       opts = {
+  --         fast_wrap = {},
+  --         disable_filetype = { "TelescopePrompt", "vim" },
+  --       },
+  --       config = function(_, opts)
+  --         require("nvim-autopairs").setup(opts)
+  --
+  --         -- setup cmp for autopairs
+  --         local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  --         require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  --       end,
+  --     },
+  --
+  --     -- cmp sources plugins
+  --     {
+  --       "saadparwaiz1/cmp_luasnip",
+  --       "hrsh7th/cmp-nvim-lua",
+  --       "hrsh7th/cmp-nvim-lsp",
+  --       "hrsh7th/cmp-buffer",
+  --       "hrsh7th/cmp-path",
+  --     },
+  --   },
+  --   opts = function()
+  --     local cmp = require('cmp')
+  --
+  --     return {
+  --       snippet = {
+  --         -- REQUIRED - you must specify a snippet engine
+  --         expand = function(args)
+  --           vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+  --           -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+  --           -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+  --           -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+  --         end,
+  --       },
+  --       window = {
+  --         -- completion = cmp.config.window.bordered(),
+  --         -- documentation = cmp.config.window.bordered(),
+  --       },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  --         ['<C-Space>'] = cmp.mapping.complete(),
+  --         ['<C-e>'] = cmp.mapping.abort(),
+  --         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --       }),
+  --       sources = cmp.config.sources({
+  --         { name = 'nvim_lsp' },
+  --         { name = 'vsnip' }, -- For vsnip users.
+  --         -- { name = 'luasnip' }, -- For luasnip users.
+  --         -- { name = 'ultisnips' }, -- For ultisnips users.
+  --         -- { name = 'snippy' }, -- For snippy users.
+  --       }, {
+  --         { name = 'buffer' },
+  --       })
+  --     }
+  --   end,
+  --   config = function(_, opts)
+  --     require('cmp').setup(opts)
+  --   end,
+  -- },
   {
     'ms-jpq/coq_nvim',
     branch = "coq",
@@ -112,11 +190,11 @@ return {
   },
 
   -- auto pairs
-  {
-    'echasnovski/mini.pairs',
-    event = 'VeryLazy',
-    opts = {},
-  },
+  -- {
+  --   'echasnovski/mini.pairs',
+  --   event = 'VeryLazy',
+  --   opts = {},
+  -- },
 
   -- Fast and feature-rich surround actions. For text that includes
   -- surroundin characters like brackets or quotes, this allows you
