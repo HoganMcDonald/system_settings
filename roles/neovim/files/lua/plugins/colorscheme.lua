4d7e634a5cfed3f8ad9d423ef0fbe537fee4cca0
Hogan McDonald <hogan.developer@gmail.com>
2024-02-24 20:39:34 -0600

    gives up on base 16

--- roles/neovim/files/lua/plugins/colorscheme.lua
+++ roles/neovim/files/lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
   {
     'kwsp/halcyon-neovim',
     lazy = false,
-    config = function(_, opts)
+    config = function()
       vim.cmd.colorscheme('halcyon')
 
       -- fixes issue where highlights for vertsplit seem backwards
@@ -15,33 +15,4 @@ return {
       fg('VertSplit', Colors.GREY)
     end,
   },
-  --
-  -- -- halcyon base16 custom
-  -- {
-  --   'RRethy/base16-nvim',
-  --   lazy = false,
-  --   config = function()
-  --     require('base16-colorscheme').setup({
-  --       base00 = Colors.BACKGROUND, -- background
-  --       base01 = Colors.WORKBENCH,
-  --       base02 = Colors.GREY,
-  --       base03 = Colors.UI_GREY,
-  --       base04 = '#565c84',
-  --       base05 = '#abb2bf',
-  --       base06 = Colors.LIGHT_GREY,
-  --       base07 = Colors.WHITE,
-  --       base08 = Colors.ACCENT,
-  --       base09 = Colors.ORANGE,
-  --       base0A = Colors.YELLOW,
-  --       base0B = Colors.GREEN,
-  --       base0C = Colors.PURPLE,
-  --       base0D = Colors.RED,
-  --       base0E = Colors.BLUE,
-  --       base0F = '#bada55',
-  --     })
-  --
-  --     bg('VertSplit', Colors.BACKGROUND)
-  --     fg('VertSplit', Colors.GREY)
-  --   end,
-  -- },
 }
