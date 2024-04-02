e8a353ccf7b7ae3719a5c06147d9000d697139e8
Hogan McDonald <hogan.developer@gmail.com>
2024-04-02 13:30:44 -0500

    stylua

--- roles/neovim/files/lua/plugins/lsp/init.lua
+++ roles/neovim/files/lua/plugins/lsp/init.lua
@@ -1,12 +1,12 @@
-local Colors = require("util.colors")
+local Colors = require 'util.colors'
 
 return {
   -- lsp installer
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
     },
   },
 
@@ -15,9 +15,9 @@ return {
   -- ---------------------
   -- free up ram by stopping lsps
   {
-    "zeioth/garbage-day.nvim",
-    dependencies = "neovim/nvim-lspconfig",
-    event = "VeryLazy",
+    'zeioth/garbage-day.nvim',
+    dependencies = 'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
     opts = {
       -- your options here
     },
@@ -28,123 +28,123 @@ return {
   -- ---------------------
   -- lsp progress
   {
-    "j-hui/fidget.nvim",
+    'j-hui/fidget.nvim',
   },
 
   -- virtual text lines
   {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
     config = function()
-      require("lsp_lines").setup()
+      require('lsp_lines').setup()
     end,
   },
 
   -- lsp hover
   {
-    "lewis6991/hover.nvim",
+    'lewis6991/hover.nvim',
     keys = {
       {
-        "K",
+        'K',
         function()
-          require("hover").hover()
+          require('hover').hover()
         end,
-        desc = "[hover.nvim] LSP Hover",
+        desc = '[hover.nvim] LSP Hover',
       },
       {
-        "gK",
+        'gK',
         function()
-          require("hover").hover_select()
+          require('hover').hover_select()
         end,
-        desc = "[hover.nvim] LSP Hover (select)",
+        desc = '[hover.nvim] LSP Hover (select)',
       },
       {
-        "<C-p>",
+        '<C-p>',
         function()
-          require("hover").hover_switch("previous")
+          require('hover').hover_switch 'previous'
         end,
-        desc = "[hover.nvim] Previous Source",
+        desc = '[hover.nvim] Previous Source',
       },
       {
-        "<C-n>",
+        '<C-n>',
         function()
-          require("hover").hover_switch("next")
+          require('hover').hover_switch 'next'
         end,
-        desc = "[hover.nvim] Next Source",
+        desc = '[hover.nvim] Next Source',
       },
     },
     opts = {
       init = function()
-        require("hover.providers.lsp")
+        require 'hover.providers.lsp'
         -- require('hover.providers.gh')
         -- require('hover.providers.gh_user')
         -- require('hover.providers.jira')
-        require("hover.providers.man")
-        require("hover.providers.dictionary")
+        require 'hover.providers.man'
+        require 'hover.providers.dictionary'
       end,
     },
   },
 
   -- code actions previews
   {
-    "aznhe21/actions-preview.nvim",
+    'aznhe21/actions-preview.nvim',
     keys = {
       {
-        "<leader>ca",
+        '<leader>ca',
         function()
-          require("actions-preview").code_actions()
+          require('actions-preview').code_actions()
         end,
-        desc = "Code Action",
-        mode = { "n", "v" },
+        desc = 'Code Action',
+        mode = { 'n', 'v' },
       },
     },
   },
 
   -- winbar
   {
-    "SmiteshP/nvim-navic", -- statusline/winbar component using lsp
-    dependencies = "neovim/nvim-lspconfig",
+    'SmiteshP/nvim-navic', -- statusline/winbar component using lsp
+    dependencies = 'neovim/nvim-lspconfig',
     opts = {
       highlight = true,
-      separator = " 〉",
+      separator = ' 〉',
       -- VScode-like icons
       icons = {
-        File = " ",
-        Module = " ",
-        Namespace = " ",
-        Package = " ",
-        Class = " ",
-        Method = " ",
-        Property = " ",
-        Field = " ",
-        Constructor = " ",
-        Enum = " ",
-        Interface = " ",
-        Function = " ",
-        Variable = " ",
-        Constant = " ",
-        String = " ",
-        Number = " ",
-        Boolean = " ",
-        Array = " ",
-        Object = " ",
-        Key = " ",
-        Null = " ",
-        EnumMember = " ",
-        Struct = " ",
-        Event = " ",
-        Operator = " ",
-        TypeParameter = " ",
+        File = ' ',
+        Module = ' ',
+        Namespace = ' ',
+        Package = ' ',
+        Class = ' ',
+        Method = ' ',
+        Property = ' ',
+        Field = ' ',
+        Constructor = ' ',
+        Enum = ' ',
+        Interface = ' ',
+        Function = ' ',
+        Variable = ' ',
+        Constant = ' ',
+        String = ' ',
+        Number = ' ',
+        Boolean = ' ',
+        Array = ' ',
+        Object = ' ',
+        Key = ' ',
+        Null = ' ',
+        EnumMember = ' ',
+        Struct = ' ',
+        Event = ' ',
+        Operator = ' ',
+        TypeParameter = ' ',
       },
     },
   },
 
   {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
     dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons', -- optional dependency
     },
     opts = {
       theme = {
