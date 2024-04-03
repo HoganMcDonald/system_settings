ecd33fce69baee8fe897634bef0c442fd45b8bae
Hogan McDonald <hogan.developer@gmail.com>
2024-04-03 10:01:27 -0500

    fixes issues with code formatters

--- roles/neovim/files/lua/plugins/coding.lua
+++ roles/neovim/files/lua/plugins/coding.lua
@@ -178,15 +178,16 @@ return {
       },
     },
     opts = {
+      log_level = vim.log.levels.DEBUG,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform will run multiple formatters sequentially
         python = { 'isort', 'black' },
         -- Use a sub-list to run only the first available formatter
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        javascriptreact = { { 'prettierd', 'prettier' } },
-        typescriptreact = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
       },
     },
   },
