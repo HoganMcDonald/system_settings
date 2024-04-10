31047af423a3ba8400efcd76be918a0fc37422a5
Hogan McDonald <hogan.developer@gmail.com>
2024-04-10 13:46:50 -0500

    adds utility to get ip address

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -9,9 +9,14 @@ alias top='htop'
 alias man='tldr'
 alias t='tmuxinator'
 
+# utility
+alias ip="ifconfig -u | grep 'inet ' | grep -v 127.0.0.1 | cut -d\  -f2 | head -1"
+
+# neovim
 alias vim='nvim'
 alias v='nvim'
 alias vi='nvim'
 alias be='bundle exec'
 
+# ruby
 alias rubocop='bundle exec rubocop'
