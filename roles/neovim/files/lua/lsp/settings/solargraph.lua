dfc3d4bf9a7f6abf4c3f934b7b3a9fd61e4cb0d8
Hogan McDonald <hogan.developer@gmail.com>
2024-06-04 21:39:11 -0500

    adds debounce for solargraph

--- roles/neovim/files/lua/lsp/settings/solargraph.lua
+++ roles/neovim/files/lua/lsp/settings/solargraph.lua
@@ -3,6 +3,9 @@ return {
     Solargraph = {
       root_dir = require('lspconfig').util.root_pattern('Gemfile', '.git')(fname) or vim.fn.getcwd(),
       useBundler = true,
+      flags = {
+        debounce_text_changes = 150,
+      },
     },
   },
 }
