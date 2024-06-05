0083c2ad735ebc1a95bb602c3d5c1f09dfc7f5c0
Hogan McDonald <hogan.developer@gmail.com>
2024-06-04 21:39:54 -0500

    adds different code action ui with predicable keymaps

--- roles/neovim/files/lua/plugins/lsp/init.lua
+++ roles/neovim/files/lua/plugins/lsp/init.lua
@@ -113,17 +113,73 @@ return {
 
   -- code actions previews
   {
-    'aznhe21/actions-preview.nvim',
-    keys = {
-      {
-        '<leader>ca',
-        function()
-          require('actions-preview').code_actions()
-        end,
-        desc = 'Code Action',
-        mode = { 'n', 'v' },
-      },
-    },
+    'luckasRanarison/clear-action.nvim',
+    event = 'LspAttach',
+    opts = function()
+      local fmt = require('util.icons').fmt
+
+      return {
+        silent = true, -- dismiss code action requests errors
+        signs = {
+          enable = false,
+          combine = true, -- combines all action kinds into a single sign
+          priority = 200, -- extmark priority
+          position = 'eol', -- "right_align" | "overlay" | "eol"
+          separator = ' ', -- signs separator
+          show_count = false, -- show the number of each action kind
+          show_label = true, -- show the string returned by `label_fmt`
+          label_fmt = function(actions)
+            return actions[1].title
+          end, -- actions is an array of `CodeAction`
+          update_on_insert = false, -- show and update signs in insert mode
+          icons = {
+            quickfix = '🔧',
+            refactor = '💡',
+            source = '🔗',
+            combined = '💡', -- used when combine is set to true or as a fallback when there is no action kind
+          },
+          highlights = { -- highlight groups
+            quickfix = 'NonText',
+            refactor = 'NonText',
+            source = 'NonText',
+            combined = 'NonText',
+            label = 'NonText',
+          },
+        },
+        popup = { -- replaces the default prompt when selecting code actions
+          enable = true,
+          center = false,
+          border = 'rounded',
+          hide_cursor = true,
+          hide_client = false, -- hide displaying name of LSP client
+          highlights = {
+            header = 'CodeActionHeader',
+            label = 'CodeActionLabel',
+            title = 'CodeActionTitle',
+          },
+        },
+        mappings = {
+          code_action = { '<leader>ca', fmt('Fix', 'Code action') },
+          apply_first = { '<leader>cc', fmt('Fix', 'Apply') },
+          quickfix = { '<leader>cq', fmt('Fix', 'Quickfix') },
+          quickfix_next = { '<leader>cn', fmt('Fix', 'Quickfix next') },
+          quickfix_prev = { '<leader>cp', fmt('Fix', 'Quickfix prev') },
+          refactor = { '<leader>cr', fmt('Fix', 'Refactor') },
+          refactor_inline = { '<leader>cR', fmt('Fix', 'Refactor inline') },
+          actions = {
+            ['rust_analyzer'] = {
+              ['Import'] = { '<leader>ai', fmt('Fix', 'Import') },
+              ['Replace if'] = { '<leader>am', fmt('Fix', 'Replace if with match') },
+              ['Fill match'] = { '<leader>af', fmt('Fix', 'Fill match arms') },
+              ['Wrap'] = { '<leader>aw', fmt('Fix', 'Wrap') },
+              ['Insert `mod'] = { '<leader>aM', fmt('Fix', 'Insert mod') },
+              ['Insert `pub'] = { '<leader>aP', fmt('Fix', 'Insert pub mod') },
+              ['Add braces'] = { '<leader>ab', fmt('Fix', 'Add braces') },
+            },
+          },
+        },
+      }
+    end,
   },
 
   -- winbar
