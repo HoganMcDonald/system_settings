6fb61b9988b271b35a15061c93a75dcc6f02fc76
Hogan McDonald <hogan.developer@gmail.com>
2024-07-08 19:56:35 -0500

    updates zshrc

--- roles/zsh/includes.zsh
+++ roles/zsh/includes.zsh
@@ -1,3 +1,3 @@
 # ASDF
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
+# . $HOME/.asdf/asdf.sh
+# . $HOME/.asdf/completions/asdf.bash
--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -1,5 +1,7 @@
 export PATH=${PATH}:$ZSH/bin
 export PATH=$PATH:/$HOME/.dotfiles/bin
+# export PATH="/Users/hogan.mcdonald/.local/bin"
+export PATH="/Users/hogan.mcdonald/.local/bin:$PATH"
 
 function print_path() {
     echo $PATH | sed $'s/:/\\\n/g'
