41ed4b984defe9e46910e72df1a9a7a5862d5668
Hogan McDonald <hogan.developer@gmail.com>
2024-09-18 20:07:32 -0500

    fixes some deprecation warnings

--- roles/neovim/files/lua/lsp/handlers.lua
+++ roles/neovim/files/lua/lsp/handlers.lua
@@ -8,14 +8,12 @@ M.setup = function()
     { name = 'DiagnosticSignInfo', text = '' },
   }
 
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
-  end
-
   vim.diagnostic.config {
     virtual_text = false, -- virtual text
     virtual_lines = false, -- lsp lines
-    signs = true, -- sign column
+    signs = {
+      active = signs,
+    },
     underline = true, -- underlines
     update_in_insert = false,
     severity_sort = true,
