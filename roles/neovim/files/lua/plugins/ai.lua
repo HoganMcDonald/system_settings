08e62328746f9997ca2a08855463685eb6db85fb
Hogan McDonald <hogan.developer@gmail.com>
2024-09-23 10:43:45 -0500

    turns off autosuggestions in avante

--- roles/neovim/files/lua/plugins/ai.lua
+++ roles/neovim/files/lua/plugins/ai.lua
@@ -20,7 +20,7 @@ return {
       ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
       provider = 'claude', -- Recommend using Claude
       behaviour = {
-        auto_suggestions = true, -- Experimental stage
+        auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = true,
