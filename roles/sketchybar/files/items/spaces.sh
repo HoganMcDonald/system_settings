401f76df5c7a162e2b3d81a4d15f2e1537e9339c
Hogan McDonald <hogan.developer@gmail.com>
2025-02-07 16:53:50 -0600

    refactor(wm): migrate from alt to cmd-based keybindings
    BREAKING CHANGE: Window management keybindings now use cmd instead of alt as the primary modifier
    
    - Replace alt with cmd for core window operations (focus, move, resize)
    - Remove unused application launcher shortcuts
    - Integrate sketchybar with aerospace workspace events
    - Clean up and simplify sketchybar configuration

--- roles/sketchybar/files/items/spaces.sh
+++ roles/sketchybar/files/items/spaces.sh
@@ -1,48 +1,35 @@
 #!/usr/bin/env sh
 
-SPACE_ICONS=("work" "code" "dotfiles" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15")
-
-# Destroy space on right click, focus space on left click.
-# New space by left clicking separator (>)
-
-sid=0
-spaces=()
-for i in "${!SPACE_ICONS[@]}"
-do
-  sid=$(($i+1))
-  sketchybar --add space      space.$sid left                               \
-             --set space.$sid associated_space=$sid                         \
-                              icon=${SPACE_ICONS[i]}                        \
-                              icon.padding_left=10                          \
-                              icon.padding_right=15                         \
-                              background.padding_left=2                     \
-                              background.padding_right=2                    \
-                              label.padding_right=20                        \
+sketchybar --add event aerospace_workspace_change
+RED=0xffed8796
+for sid in $(aerospace list-workspaces --all); do
+    sketchybar --add item "space.$sid" left \
+        --subscribe "space.$sid" aerospace_workspace_change \
+        --set "space.$sid" \
+        icon="$sid"\
+                              icon.padding_left=22                          \
+                              icon.padding_right=22                         \
+                              label.padding_right=33                        \
                               icon.highlight_color=$RED                     \
+                              background.color=0x44ffffff \
+                              background.corner_radius=5 \
+                              background.height=30 \
+                              background.drawing=off                         \
                               label.font="sketchybar-app-font:Regular:16.0" \
-                              label.background.height=26                    \
+                              label.background.height=30                    \
                               label.background.drawing=on                   \
-                              label.background.color=$BACKGROUND_2          \
-                              label.background.corner_radius=8              \
+                              label.background.color=0xff494d64             \
+                              label.background.corner_radius=9              \
                               label.drawing=off                             \
-                              script="$PLUGIN_DIR/space.sh"                 \
-            --subscribe       space.$sid mouse.clicked
+        click_script="aerospace workspace $sid" \
+        script="$CONFIG_DIR/plugins/aerospacer.sh $sid"
 done
 
-sketchybar --add bracket spaces '/space\..*/'                  \
-           --set spaces  background.color=$BACKGROUND_1        \
-                         background.border_color=$BACKGROUND_2 \
-                         background.border_width=2             \
-                         background.drawing=on
-
-
-sketchybar   --add item       separator left                                  \
-             --set separator  icon=                                          \
-                              icon.font="Hack Nerd Font:Regular:16.0"         \
-                              background.padding_left=17                      \
-                              background.padding_right=10                     \
-                              label.drawing=off                               \
-                              associated_display=active                       \
-                              click_script='yabai -m space --create
-                                            sketchybar --trigger space_change'\
+sketchybar   --add item       separator left                          \
+             --set separator  icon=                                  \
+                              icon.font="Hack Nerd Font:Regular:16.0" \
+                              background.padding_left=15              \
+                              background.padding_right=15             \
+                              label.drawing=off                       \
+                              associated_display=active               \
                               icon.color=$WHITE
