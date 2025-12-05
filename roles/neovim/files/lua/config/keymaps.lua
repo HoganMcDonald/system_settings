5c6a5b36f995ad14c266ea03a25e07426244dd98
Hogan McDonald <hogan.developer@gmail.com>
2025-12-05 09:20:05 -0600

    feat: adds keymap to copy file name to clipboard

--- roles/neovim/files/lua/config/keymaps.lua
+++ roles/neovim/files/lua/config/keymaps.lua
@@ -2,6 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Copy current file path to clipboard
+vim.keymap.set("n", "<leader>fy", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied to clipboard: " .. path, vim.log.levels.INFO)
+end, { desc = "Copy file path" })
+
 -- Diffview keybindings (override any defaults)
 vim.keymap.set("n", "<leader>gd", function()
   require("diffview").open()
