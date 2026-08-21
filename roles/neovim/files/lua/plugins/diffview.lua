2ee94d1955fd035387222718251d7e0baed07def
Hogan McDonald <hogan.developer@gmail.com>
2026-08-21 11:19:00 -0500

    feat(neovim): add diffview.nvim
    Lazy on the Diffview commands, with <leader>gv to toggle a diff view and
    <leader>gV for the current file's history.

---
+++ roles/neovim/files/lua/plugins/diffview.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewOpen",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
+    },
+    keys = {
+      {
+        "<leader>gv",
+        function()
+          if require("diffview.lib").get_current_view() then
+            vim.cmd("DiffviewClose")
+          else
+            vim.cmd("DiffviewOpen")
+          end
+        end,
+        desc = "Diffview (toggle)",
+      },
+      { "<leader>gV", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview file history" },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+        },
+      },
+      file_panel = {
+        listing_style = "tree",
+        win_config = {
+          position = "left",
+          width = 35,
+        },
+      },
+    },
+  },
+}
